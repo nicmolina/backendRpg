cdc467bc186af8f57de8adf0b9789475304571cd
nicolas_molina <nicolas.marcondes@oowlish.com>
2021-10-17 01:53:14 -0300

    user model

---
+++ src/models/user.ts
@@ -0,0 +1,39 @@
+const mongoose = require("../database");
+const bcryptProccess = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  userId: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+const UserFile = mongoose.model("User", UserSchema);
+
+module.exports = UserFile;
